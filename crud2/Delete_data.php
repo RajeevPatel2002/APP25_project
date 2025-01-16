96e4d1a4862015dce3acd2d9fe4ef0e7f0ccfaea
RajeevPatel2002 <patelrajeev223344@gmail.com>
2025-01-16 10:42:43 +0530

    session and cookies

---
+++ crud2/Delete_data.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])){
+    header('Location:fetch_data.php');
+}
+
+
+
+
+
+
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+    $conn = mysqli_connect("localhost","root","","taskwork");
+    $sql = "Delete from data_insert where id=$id";
+    if(mysqli_query($conn,$sql)){
+        echo "<script>
+        alert('Your data delete Successfully...');
+        window.location.href='fetch_data.php';
+        </script>";
+    }
+    else{
+        header("Location:fetch_data.php");
+    }
+}
+?>
